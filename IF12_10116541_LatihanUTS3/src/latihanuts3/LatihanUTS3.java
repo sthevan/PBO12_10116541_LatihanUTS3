7fc1d831bc8017ce241c1949c0db50a5a0faa1c3
sthevan <32483662+sthevan@users.noreply.github.com>
2017-11-14 22:50:12 +0700

    Add files via upload

---
+++ IF12_10116541_LatihanUTS3/src/latihanuts3/LatihanUTS3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanuts3;
+
+/**
+ *
+ * @author STHEVAN REYNALDO SIMAK LANDO (10116541) PBO12
+ */
+public class LatihanUTS3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
